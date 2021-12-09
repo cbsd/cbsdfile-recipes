0887504d2a405521d623e3b84f1ac2c9bd645a5e
Charlie Root <root@mother.olevole.ru>
2021-12-09 22:36:33 +0300

    login form test

---
+++ jail/gitlab/tests/login.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+
+CURL_CMD=$( which curl 2>/dev/null )
+
+if [ -z "${CURL_CMD}" ]; then
+	echo "no such curl"
+	exit 1
+fi
+
+ip4_addr=$( cbsd jget jname=gitlab mode=quiet ip4_addr 2>/dev/null )
+GREP_VAL=""form.*action.*post""
+
+printf "Check for login page http://${ip4_addr}/users/sign_in ( filter cmd: ${GREP_VAL} )..." 2>&1
+
+${CURL_CMD} --no-progress-meter -L http://${ip4_addr}/users/sign_in | grep "${GREP_VAL}"
+ret=$?
+
+exit ${ret}
--- jail/pgadmin4/tests/login.sh
+++ jail/pgadmin4/tests/login.sh
@@ -8,10 +8,10 @@ if [ -z "${CURL_CMD}" ]; then
 	exit 1
 fi
 
-ip4_addr=$( cbsd jget jname=pgadmin4 mode=quiet ip4_addr  2>/dev/null )
+ip4_addr=$( cbsd jget jname=pgadmin4 mode=quiet ip4_addr 2>/dev/null )
 
-printf "Check for login page http://${ip4_addr}:5050 ( filter cmd: ${GREP_VAL} )..." 2>&1
 GREP_VAL="submit.*Login"
+printf "Check for login page http://${ip4_addr}:5050 ( filter cmd: ${GREP_VAL} )..." 2>&1
 
 ${CURL_CMD} --no-progress-meter -L http://${ip4_addr}:5050 | grep "${GREP_VAL}"
 ret=$?
