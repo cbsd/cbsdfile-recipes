b3625c680b9837efe070878bd30742021d1f3505
root <root@srv-03.olevole.ru>
2021-12-12 21:35:43 +0300

    support for v6-only

--- jail/gitlab/tests/70_login.sh
+++ jail/gitlab/tests/70_login.sh
@@ -11,9 +11,17 @@ fi
 ip4_addr=$( cbsd jget jname=gitlab mode=quiet ip4_addr 2>/dev/null )
 GREP_VAL=""form.*action.*post""
 
-printf "Check for login page http://${ip4_addr}/users/sign_in ( filter cmd: ${GREP_VAL} )..." 2>&1
-
-${CURL_CMD} --no-progress-meter -L http://${ip4_addr}/users/sign_in | grep "${GREP_VAL}"
-ret=$?
+case "${ip4_addr}" in
+	*\.*\.*\.*)
+		printf "Check for login page http://${ip4_addr}/users/sign_in ( filter cmd: ${GREP_VAL} )..." 2>&1
+		${CURL_CMD} --no-progress-meter -L http://${ip4_addr}/users/sign_in | grep "${GREP_VAL}"
+		ret=$?
+		;;
+	*:*)
+		printf "Check for login page http://[${ip4_addr}]/users/sign_in ( filter cmd: ${GREP_VAL} )..." 2>&1
+		${CURL_CMD} -6 --no-progress-meter -L http://[${ip4_addr}]/users/sign_in | grep "${GREP_VAL}"
+		ret=$?
+		;;
+esac
 
 exit ${ret}
