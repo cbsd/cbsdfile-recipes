26e817f7308a68de515e3b127b5ed708b97f6a19
olevole <olevole@srv-01.olevole.ru>
2022-02-01 23:11:21 +0300

    wip

--- jail/gitlab/tests/50_nginx_process.sh
+++ jail/gitlab/tests/50_nginx_process.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
-pid=$( cbsd jexec jname=gitlab pgrep nginx 2>/dev/null | grep . | awk '{printf $1}' )
+pid=$( cbsd jexec jname=gitlab pgrep nginx 2>/dev/null | grep . | awk '{printf $1" "}' )
 
 if [ -n "${pid}" ]; then
 	echo "nginx process exist: ${pid}" 2>&1
