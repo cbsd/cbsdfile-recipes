337ec7a96902313b6fc4a55c954cd20e447bb81b
Charlie Root <root@cbsdbuilder131.my.domain>
2022-09-14 19:15:03 +0300

    wip

--- jail/sambashare/tests/process.sh
+++ jail/sambashare/tests/process.sh
@@ -3,7 +3,7 @@ export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 export NOCOLOR=1
 
 [ -z "${jname}" ] && jname="sambashare"
-pid=$( cbsd jexec jname=${jname} pgrep smbd 2>/dev/null | grep . | awk '{printf $1}' )
+pid=$( cbsd jexec jname=${jname} pgrep smbd 2>/dev/null | grep . | awk '{printf $1" "}' )
 
 if [ -n "${pid}" ]; then
 	echo "smbd process exist: ${pid}"
