dae0b893fab2d94f168849143141501f3e284ec6
Charlie Root <root@cbsdbuilder140.my.domain>
2022-09-15 12:05:43 +0300

    wip

--- jail/postgresql/tests/port.sh
+++ jail/postgresql/tests/port.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 export NOCOLOR=1
-[ -z "${jname}" ] && jname="postgresql13"
+[ -z "${jname}" ] && jname="postgresql"
 NC_CMD=$( which nc )
 
 if [ -z "${NC_CMD}" ]; then
--- jail/postgresql13/jails-system/bin/bootstrap.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cbsd forms module=postgresql13 jname=${jname}
