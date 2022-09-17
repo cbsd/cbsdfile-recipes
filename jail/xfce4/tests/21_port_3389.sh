7660da0c785d79246bf852f1edfdf1481d5ff036
Charlie Root <root@cbsdbuilder140.my.domain>
2022-09-17 20:54:38 +0300

    xfce4: add tests

---
+++ jail/xfce4/tests/21_port_3389.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+export NOCOLOR=1
+[ -z "${jname}" ] && jname="xfce4"
+
+NC_CMD=$( which nc )
+
+if [ -z "${NC_CMD}" ]; then
+	echo "no such nc"
+	exit 1
+fi
+
+ip4_addr=$( cbsd jget jname=${jname} mode=quiet ip4_addr )
+
+echo "Probing: ${ip4_addr}:3389" 2>&1
+${NC_CMD} -z ${ip4_addr} 3389 2>&1
+ret=$?
+
+exit ${ret}
