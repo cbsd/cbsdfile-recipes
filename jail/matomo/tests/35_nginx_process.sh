ef278657d02e67e40e7437a1413aa533668a7b0e
Charlie Root <root@cbsdbuilder140.my.domain>
2022-09-20 01:37:46 +0300

    add matomo template

---
+++ jail/matomo/tests/35_nginx_process.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+export NOCOLOR=1
+[ -z "${jname}" ] && jname="matomo"
+
+pid=$( cbsd jexec jname=${jname} pgrep nginx 2>/dev/null | grep . | awk '{printf $1" "}' )
+
+printf "Check php-fpm process... " 2>&1
+
+if [ -n "${pid}" ]; then
+	echo "${pid}" 2>&1
+	ret=0
+else
+	echo "failed" 2>&1
+	ret=1
+fi
+
+exit ${ret}
+
