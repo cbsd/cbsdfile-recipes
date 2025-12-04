292ff8062af0e2b651003a3e1029194389e6064d
olevole <olevole@ya.ru>
2025-12-04 16:44:00 +0300

    fix tests

--- jail/transmission/tests/process.sh
+++ jail/transmission/tests/process.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 export NOCOLOR=1
-[ -z "${jname}" ] && jname="qbittorrent"
+[ -z "${jname}" ] && jname="transmission"
 
-pid=$( cbsd jexec jname=${jname} pgrep qbittorrent-nox 2>/dev/null | grep . | awk '{printf $1}' )
+pid=$( cbsd jexec jname=${jname} pgrep transmission-daemon 2>/dev/null | grep . | awk '{printf $1}' )
 
 if [ -n "${pid}" ]; then
-	echo "qbittorrent-nox process exist: ${pid}"
+	echo "transmission process exist: ${pid}"
 else
-	echo "qbittorrent-nox process not exist"
+	echo "transmission process not exist"
 	exit 1
 fi
 
--- jail/transmission/tests/web.sh
+++ jail/transmission/tests/web.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 export NOCOLOR=1
-[ -z "${jname}" ] && jname="qbittorrent"
+[ -z "${jname}" ] && jname="transmission"
 
 CURL_CMD=$( which curl )
 
@@ -12,10 +12,10 @@ fi
 
 ip4_addr=$( cbsd jget jname=${jname} mode=quiet ip4_addr )
 
-GREP_VAL="(submit)*loginButton"
+GREP_VAL="open-torrent"
 
-echo "Probing: http://${ip4_addr}:8080 ( filter cmd: ${GREP_VAL} )"
-${CURL_CMD} --no-progress-meter -L http://${ip4_addr}:8080 | grep -E "${GREP_VAL}"
+echo "Probing: http://${ip4_addr}:9091 ( filter cmd: ${GREP_VAL} )"
+${CURL_CMD} --no-progress-meter -L http://${ip4_addr}:9091 | grep -E "${GREP_VAL}"
 ret=$?
 
 exit ${ret}
